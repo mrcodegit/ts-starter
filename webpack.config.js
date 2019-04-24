82b0935ac1e301d97c0d8b96f4d4b07c821bf6b3
Maciej Radziszowski <mrcodegh@gmail.com>
2019-04-24 20:57:30 +0200

    live reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/ts/main.ts',
@@ -26,6 +27,11 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
-
-}
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+};
